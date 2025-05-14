e74ecbb36a5814314750945dbf2cc445455c79fa
PuppyLo <PuppyLo@users.noreply.github.com>
2025-05-14 20:24:15 +0000

    deploy: a8d73b121a313d949c3608a948a6dcb031c562c1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"38e19d01dcac654bbb941b771cc06efd4ed4a9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "993566931"
+    serviceWorkerVersion: "1165588975"
   }
 });
