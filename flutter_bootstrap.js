1798b3ceb4fedf2cd555ee4950f8412d2db490ff
PuppyLo <PuppyLo@users.noreply.github.com>
2025-06-25 14:48:16 +0000

    deploy: f2f1403107c42a016ef43d2c5806aac8a2478a99

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -34,10 +34,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"8ed9edbd7aaadf3c101281d43579d617b859e2fe","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"8ed9edbd7aaadf3c101281d43579d617b859e2fe","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "326154063"
+    serviceWorkerVersion: "136530090"
   }
 });
